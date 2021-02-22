90c2646824efa939b429602b326e38ac74aba3f2
GhostLexly <ghostlexly@gmail.com>
2021-02-22 18:50:22 +0100

    + Add automatic project deployment

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -1,3 +1,2 @@
-
-rsync --delete -avzhe ssh immotreso@35.187.171.205:/opt/dividends-realtime-income/ /mnt/Git/dividends-realtime-income/
+rsync --delete -avh /mnt/Git/dividends-realtime-income/ immotreso@35.187.171.205:/opt/dividends-realtime-income/
 ssh -t immotreso@35.187.171.205 'cd /opt/dividends-realtime-income/ && docker-compose build && docker-compose up -d'
