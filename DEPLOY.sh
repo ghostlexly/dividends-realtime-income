b63c9d9c0c7ed64635b9977d17ec80ec68f1b26c
GhostLexly <ghostlexly@gmail.com>
2021-02-22 18:39:21 +0100

    + Add docker configuration

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -1,2 +1,3 @@
 
-rsync --delete -avzhe ssh root@35.187.171.205:/opt/dividends-realtime-income/ /opt/dividends-realtime-income/
+rsync --delete -avzhe ssh immotreso@35.187.171.205:/opt/dividends-realtime-income/ /mnt/Git/dividends-realtime-income/
+ssh -t immotreso@35.187.171.205 'cd /opt/dividends-realtime-income/ && docker-compose build && docker-compose up -d'
